57786bd3bea3228961249078355f7bfaefcc6f7f
matthieuDev <matthieu.devaux.epfl@outlook.com>
2024-11-03 18:35:41 +0100

    get_station_information

--- download_data.py
+++ download_data.py
@@ -1,7 +1,8 @@
 import requests, os, json
 from datetime import datetime
 
-def get_current_station_status(data_folder = os.path.dirname(__file__) +'/data/') : 
+data_folder = os.path.dirname(__file__) +'/data/'
+def get_current_station_status(data_folder = data_folder) : 
     if not os.path.exists(data_folder) :
         os.mkdir(data_folder)
         
@@ -25,5 +26,22 @@ def get_current_station_status(data_folder = os.path.dirname(__file__) +'/data/'
     with open(save_filename, 'w') as f:
         json.dump(j['data']['stations'], f)
 
+def get_station_information(data_folder = data_folder):
+    filepath = f'{data_folder}station_status.json'
+    url = 'https://velib-metropole-opendata.smovengo.cloud/opendata/Velib_Metropole/station_information.json'
+    if os.path.exists(filepath):
+        with open(filepath) as f:
+            return json.load(f)['data']['stations']
+    else:
+        req = requests.get(url)
+        if req.status_code == 200 :
+            station_information = req.json()
+            with open(filepath, 'w') as f:
+                json.dump(station_information, f, indent=2)
+            return station_information['data']['stations']
+        else:
+            raise Exception(f'Could not download: {req.raise_for_status()}')
+
 if __name__ =='__main__' :
     get_current_station_status()
+    get_station_information()
